c6a5e54b100da9bde00c68c4182dea7371942759
WadeSeidule <james.seidule@carta.com>
2022-12-04 20:28:49 -0500

    get input programatically

--- helpers/new_day.py
+++ helpers/new_day.py
@@ -1,26 +1,46 @@
 import shutil
-import sys
 import pathlib
 import datetime
 import os
+import requests
+import argparse
+
+CURR_DIR = pathlib.Path(__file__).parent.resolve()
+AOC_URL = "https://adventofcode.com"
+INPUT_TEMPLATE = "{url}/{year}/day/{day}/input"
+
+def get_input(year, day) -> str:
+    input_url = INPUT_TEMPLATE.format(url=AOC_URL, year=year, day=day)
+    with open(f'{CURR_DIR}/cookie.txt') as f:
+        cookie = f.read().strip()
+    res = requests.get(input_url, cookies={"session": cookie})
+    return res.text.strip()
+
 
 def main() -> None:
-    curr_dir = pathlib.Path(__file__).parent.resolve()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--day', type=int, required=True)
+    args = parser.parse_args()
+
     year = datetime.datetime.now().year
-    day = sys.argv[1]
+    day = args.day
 
     # create dir
-    folder = f"{curr_dir}/../{year}/{day}"
+    folder = f"{CURR_DIR}/../{year}/day{day}"
     if not os.path.exists(folder):
         os.makedirs(folder)
 
-    # create files
-    files_to_create = (f'{folder}/input.txt', f'{folder}/answer.txt')
-    for file in files_to_create:
-         if not os.path.exists(file):
-            open(file, 'w').close()
+    # get input and create input file
+    puzzle_input = get_input(year, day)
+    with open(f'{folder}/input.txt', 'w') as f:
+        f.write(puzzle_input)
 
-    shutil.copyfile(f'{curr_dir}/template.py', f'{folder}/{day}.py')
+    # create empty answer file for storage
+    open(f'{folder}/answer.txt', 'w').close()
+
+    # copy template python script to created folder
+    shutil.copyfile(f'{CURR_DIR}/template.py', f'{folder}/day{day}.py')
 
     print(f"created: {year}/{day}")
 
